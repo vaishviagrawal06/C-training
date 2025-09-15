e62bd7682febe4d1cdacfa240d673c99d345400a
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2025-09-15 18:38:04 +0530

    msg

---
+++ lec1/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>   
+
+int main() {
+    
+    int age = 20;          
+    float pi = 3.14;       
+    char grade = 'A';      
+
+    
+    printf("Hello Students!\n");     
+    printf("Age = %d\n", age);      
+    printf("Pi = %f\n", pi);         
+    printf("Grade = %c\n", grade);   
+
+    return 0;
+}
