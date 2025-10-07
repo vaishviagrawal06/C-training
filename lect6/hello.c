32a26af00ce519ae33d66ddd4efe0873b811c14c
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2025-10-07 09:23:02 +0530

    msg

--- lect6/hello.c
+++ lect6/hello.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
+// 1
+// 7 2
+// 12 8 3
+// 16 13 9 4
+// 19 17 14 10 5
+// 21 20 18 15 11 6
 
-int main() {
- int n=5;
- for(int i =0;i<n;i++)
- {
-   for(int j=0;j<n-i;j++)
-   {
-    printf("* ");
-   }
-   printf("\n");
- }
+
+#include<stdio.h>
+int main(){
+    int n=6;
+    int start = 1;
+    for(int r=0;r<n;r++){
+        int num = start;
+        for(int c=0;c<=r;c++){
+            printf("%d",num);
+            if(c<r)
+            {
+                num-=(n-r+c);
+                printf(" ");
+            }
+        }
+        printf("\n");
+        start+=(n-(r-0));
+    }
     return 0;
-
 }
--- lect6/pattern of differnece in decreasing order by instagram.c
+++ lect6/pattern of differnece in decreasing order by instagram.c
@@ -1,5 +1,13 @@
+// 1
+// 7 2
+// 12 8 3
+// 16 13 9 4
+// 19 17 14 10 5
+// 21 20 18 15 11 6
+
 #include<stdio.h>
 int main(){
+
     int n=6;
     int start = 1;
     for(int r=0;r<n;r++){
